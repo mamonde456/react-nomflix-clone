ae258d36d4e22cf0d151913c025adfa0edad4c2e
mamonde456 <mamonde456@gmail.com>
2022-05-16 16:22:09 +0900

    nomflix clone coding start

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,18 @@
-import styled from "styled-components";
-import {
-  AnimatePresence,
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
-import { useEffect, useRef, useState } from "react";
-
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  height: 200px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
-  gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
-`;
-
-const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 function App() {
-  const [id, setId] = useState<null | string>(null);
-
   return (
-    <Wrapper>
-      <Grid>
-        {["1", "2", "3", "4"].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
-        ))}
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-            animate={{ backgroundColor: "rgba(0,0,0,0.8)" }}
-            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
-          >
-            <Box layoutId={id} style={{ width: 400, height: 200 }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
-    </Wrapper>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
   );
 }
 
